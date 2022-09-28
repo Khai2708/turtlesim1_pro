65dcfe28d28ff588a3503bed5184814f1bc1eecf
Khai2708 <khalilovasadbek27@gmail.com>
2022-09-28 15:59:20 +0900

    Week 5 activity

---
+++ install/py_pubsub/lib/python3.8/site-packages/py_pubsub/setup.py
@@ -0,0 +1,6 @@
+entry_points={
+        'console_scripts': [
+                'talker = py_pubsub.publisher_member_function:main',
+                'listener = py_pubsub.subscriber_member_function:main',
+        ],
+},
