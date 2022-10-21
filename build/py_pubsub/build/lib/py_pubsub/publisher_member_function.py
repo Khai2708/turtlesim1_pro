e7b00116d36e93a3c317e772ccb70e27ecfa82e2
Khai2708 <khalilovasadbek27@gmail.com>
2022-10-21 09:09:41 +0900

    ver1

--- build/py_pubsub/build/lib/py_pubsub/publisher_member_function.py
+++ build/py_pubsub/build/lib/py_pubsub/publisher_member_function.py
@@ -15,24 +15,24 @@
 import rclpy
 from rclpy.node import Node
 
-from std_msgs.msg import String
+from tutorial_interfaces.msg import Num    # CHANGE
 
 
 class MinimalPublisher(Node):
 
     def __init__(self):
         super().__init__('minimal_publisher')
-        self.publisher_ = self.create_publisher(String, 'topic', 10)
+        self.publisher_ = self.create_publisher(Num, 'topic', 10)  # CHANGE
         timer_period = 0.5  # seconds
         self.timer = self.create_timer(timer_period, self.timer_callback)
         self.i = 0
 
     def timer_callback(self):
-        msg = String()
-        msg.data = 'Hello World: %d' % self.i
-        self.publisher_.publish(msg)
-        self.get_logger().info('Publishing: "%s"' % msg.data)
-        self.i += 1
+         msg = Num()                                           # CHANGE
+         msg.num = self.i                                      # CHANGE
+         self.publisher_.publish(msg)
+         self.get_logger().info('Publishing: "%d"' % msg.num)  # CHANGE
+         self.i += 1
 
 
 def main(args=None):
